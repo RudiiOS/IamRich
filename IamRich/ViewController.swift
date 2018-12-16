6dfdc6862fc83cfecda107e9ea334148ae054227
Rudi_iOS <39303546+RudiiOS@users.noreply.github.com>
2018-12-16 22:21:40 +0100

    Add files via upload

---
+++ IamRich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IamRich
+//
+//  Created by Leart on 12/15/18.
+//  Copyright © 2018 RudiLeart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
